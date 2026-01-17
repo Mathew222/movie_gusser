348b59bdd3accf9b871e81e60dfe3ffda3a32ae1
Mathew222 <mathewbinu2004@gmail.com>
2026-01-17 22:21:41 +0530

    chore: bump version to 1.2.0

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@
  * A fun movie guessing game with multiple difficulty levels,
  * hints system, and timer mode for challenging gameplay.
  * 
- * @version 1.1.0
+ * @version 1.2.0
  * @author Movie Guess Team
  */
 
