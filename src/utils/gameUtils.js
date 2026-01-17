63de8c054daa4eac0654ac70fde0b9ea702dc6e6
Mathew222 <mathewbinu2004@gmail.com>
2026-01-17 22:22:09 +0530

    feat: add shuffleArray utility function

--- src/utils/gameUtils.js
+++ src/utils/gameUtils.js
@@ -123,3 +123,17 @@ export const calculateStreakBonus = (streak) => {
   if (streak >= 3) return 5
   return 2
 }
+
+/**
+ * Shuffle an array using Fisher-Yates algorithm
+ * @param {Array} array - The array to shuffle
+ * @returns {Array} - A new shuffled array
+ */
+export const shuffleArray = (array) => {
+  const shuffled = [...array]
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+  }
+  return shuffled
+}
